ad31257eda7f24aaa52429e5493a8b448196512e
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-23 22:51:23 -0400

    Starting a second Liri Bot repo with out keyes.

---
+++ liri.js
@@ -0,0 +1,22 @@
+// console.log("Node Testing Done Here");
+
+// var Twitter = require('twitter');
+
+// var config = require('./keys2');
+
+// var T = new Twitter(config);
+
+
+
+//   var params = {q: 'JSON_NAVARRO'};
+//   T.get('search/tweets', params, function(error, tweets, response) {
+//     if (!error) {
+//       console.log(tweets.text);
+     
+//     }
+//     if(error){
+//         console.log(error);
+//     }
+//   });
+
+
