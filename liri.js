f0a60664055a1d6045b93afb332a70b99d5567ac
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-23 23:17:45 -0400

    Able to get JSON from twitter and add different params.

--- liri.js
+++ liri.js
@@ -1,22 +1,22 @@
-// console.log("Node Testing Done Here");
+console.log("Node Testing Done Here");
 
-// var Twitter = require('twitter');
+var Twitter = require('twitter');
 
-// var config = require('./keys2');
+var config = require('./keys2');
 
-// var T = new Twitter(config);
+var T = new Twitter(config);
 
 
 
-//   var params = {q: 'JSON_NAVARRO'};
-//   T.get('search/tweets', params, function(error, tweets, response) {
-//     if (!error) {
-//       console.log(tweets.text);
+  var params = {q: 'JSON_NAVARRO',result_type: "recent", count: 20};
+  T.get('search/tweets', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
      
-//     }
-//     if(error){
-//         console.log(error);
-//     }
-//   });
+    }
+    if(error){
+        console.log(error);
+    }
+  });
 
 
