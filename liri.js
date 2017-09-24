9477aa648fe898a38f1f42f5a5cc56c7a42a9090
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-23 23:56:04 -0400

    figuered out how to get just the tweet to pop up from the JSON.

--- liri.js
+++ liri.js
@@ -8,12 +8,27 @@ var T = new Twitter(config);
 
 
 
-  var params = {q: 'JSON_NAVARRO',result_type: "recent", count: 20};
-  T.get('search/tweets', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
+  var params = {q: 'JSON_NAVARRO',result_type: "recent", count: 20, text: "text"};
+
+//   T.get('search/tweets', params, function(error, tweets, response) {
+//     if (!error) {
+//       console.log("search", tweets);
      
+//     }
+//     if(error){
+//         console.log(error);
+//     }
+//   });
+
+
+  T.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+
+
+      console.log("statuses",tweets[0].text);
+   
     }
+
     if(error){
         console.log(error);
     }
