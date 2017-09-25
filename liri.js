35fca577fca633e2f7b52b976f5bf21eb47f9d5b
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-24 20:06:54 -0400

    Finished with spotify.

--- liri.js
+++ liri.js
@@ -1,5 +1,3 @@
-console.log("Node Testing Done Here");
-
 var spotify = require('./spotify')
 
 var keys = require('./keys2');
@@ -10,24 +8,37 @@ var Twitter = require('twitter');
 
 var T = new Twitter(keys);
 
+var twitterName = 'JSON_NAVARRO';
+
+
+if(process.argv[2] == "my-tweets" && process.argv.length > 3){
+    twitterName = process.argv[3];
+    console.log("haha");
+}
+console.log(twitterName);
 var params = {
-    q: 'JSON_NAVARRO',
+    screen_name: twitterName,
     result_type: "recent",
     count: 20
 };
 
-// if(process.argv[2] == "my-tweets"){
-// T.get('statuses/user_timeline', params, function (error, tweets, response) {
-//     if (!error) {
+if(process.argv.length == 3 && process.argv[2] == "my-tweets"){
+    console.log("HELP - If you want to search your own TWEETS use LIRI like: node liri.js my-tweets <YOUR_SCREEN_NAME>");
+    console.log("Here is an Example of the results using my own Screen Name:");
+}
 
-//         for (var x in tweets) {
+if(process.argv[2] == "my-tweets"){
+T.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (!error) {
+
+        for (var x in tweets) {
             
-//             console.log('Tweeted: "', tweets[x].text, '" on ' + tweets[x].created_at);    
-//         }    
-//     }
-//     if (error) {
-//         console.log(error);
-//     }
-// });
+            console.log('Tweeted: "', tweets[x].text, '" on ' + tweets[x].created_at);    
+        }    
+    }
+    if (error) {
+        console.log(error);
+    }
+});
 
-// };
+};
--- spotify.js
+++ spotify.js
@@ -1,35 +1,72 @@
+// These variables require the modules and files needed for the spotify calls. 
 var spotifyApi = require('node-spotify-api');
 var spotifyKeys = require('./spotKeys');
 
-function spotifyBot(){
+// I entered all my code for the spoitfy call in this function.
+function spotifyBot() {
+if (process.argv[2] == "spotify-this-song"){
+  // I am inputing my spotify access keyes. 
+  var spotify1 = new spotifyApi(spotifyKeys);
 
+  // Here I put all words from process.argv 3 + into an array and then join them for the spotify api.
+  var stringSong = [];
 
-    var spotify1 = new spotifyApi(spotifyKeys);
-       
-      spotify1.search({ type: 'track', query: 'if i ruled the world' , limit: 1}, function(err, data) {
-        if (err) {
-          return console.log('ERROR occurred: ' + err);
-        }
-        // album name
-        console.log("Album Name: ", data.tracks.items[0].album.name);
+  // this is the for loop that pushes all the process.argv 3+ into the array.
+  for (var y = 3; y < process.argv.length; y++) {
+    stringSong.push(process.argv[y]);
+  }
+  // this joins the array StringSong. 
+  var dynamicString = stringSong.join(' ')
 
-        // song name
-         console.log("Song Name: ", data.tracks.items[0].name);
+// This is my defualt if no song name is entered when using spotify-this-song in the command line.
+  if (process.argv[2] == "spotify-this-song" && process.argv.length == 3) {
+    dynamicString = "The Sign";
+  }
 
-        //  artist name
-        for(var i = 0; i < data.tracks.items[0].artists.length; i++){
-        console.log("Artist: ", data.tracks.items[0].artists[i].name);
-        }
-      // console.log("Link", data.tracks.items[0].external_urls.spotify); 
-      
-      });
+  // This is the params for the spotify node api. 
+  if (process.argv[2] == "spotify-this-song") {
+    var params2 = {
+      type: 'track',
+      query: dynamicString,
+      limit: 20
+    };
+  }
 
+// This is the code to search the spotify API and retrive a JSON.
+  spotify1.search(params2, function (err, data) {
+    if (err) {
+      return console.log('ERROR occurred: ' + err);
+    }
 
+// Here I set this variable to search the JSON.
+    var getJSON = data.tracks.items[0];
 
+    // Here I set the instructions and example if no song-name is entered. 
+    if (process.argv[2] == "spotify-this-song" && process.argv.length == 3) {
+      getJSON = data.tracks.items[18];
+
+      console.log("HELP - To search for songs use LIRI like so:  node liri.js spotify-this-song '<song name here>'");
+      console.log("If we search 'The Sign' Results will look like:");
+    }
+
+    // This gets the song name
+    console.log("We found this info for -", getJSON.name);
+    console.log("The Artist on this track include: ");
+    //  This gets the artist name
+    for (var i = 0; i < getJSON.artists.length; i++) {
+      console.log(getJSON.artists[i].name);
+    }
+    // This gets the album name
+    console.log("Album Name: ", getJSON.album.name);
+
+    // This gets the link to listen to the song on spotifiy. 
+    console.log("Listen to the Song now:", getJSON.external_urls.spotify);
+  });
 
 }
+}
 
-
+// Here I send the function to my main file liri.js.
 module.exports = {
-    spotifyBot: spotifyBot(),
+  spotifyBot: spotifyBot(),
 }
